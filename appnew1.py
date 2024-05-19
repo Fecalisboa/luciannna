f733747882a6a26aa11611df68f8b59acb7c75ff
Fecalisboa <127762860+Fecalisboa@users.noreply.github.com>
2024-05-19 18:48:45 -0300

    appnew1.py

--- appnew1.py
+++ appnew1.py
@@ -118,7 +118,7 @@ for idx, msg in enumerate(msgs.messages):
 
 # Função para verificar se a pergunta é jurídica
 def is_legal_question(question):
-    legal_keywords = ["lei", "contrato", "jurídico", "advogado", "justiça", "processo", "direito", "tribunal","Bom dia", "Boa noite", "boa tarde", "oi", "olá", "saudações"]
+    legal_keywords = ["lei", "contrato", "jurídico", "advogado", "justiça", "processo", "direito", "tribunal"]
     return any(keyword in question.lower() for keyword in legal_keywords)
 
 # Função para o chat da IA
@@ -193,5 +193,6 @@ if option == "IA - CHAT":
     ia_chat()
 elif option == "IA - Docs":
     ia_docs()
+
 
 
